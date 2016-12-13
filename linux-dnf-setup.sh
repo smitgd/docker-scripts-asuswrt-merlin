6bbb17b2e1fab0370266f8efa21054c1fc659b5a
Gene Smit <gene@localhost.localdomain>
2016-12-13 00:05:13 -0500

    Added file for build dependency setup for dnf based system, e.g., Fedora. Also, renamed apt (Ubuntu/Debian) set up file to contain apt in name. 	renamed:    linux-setup.sh -> linux-apt-setup.sh 	new file:   linux-dnf-setup.sh

---
+++ linux-dnf-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fedora (uses dnf) build dependencies. Usage:
+# sudo ./linux-dnf-setup 
+# Do one time before running sudo ./linux-build-asuswrt-merlin.sh
+
+# need due to initial conflicts of vim-minimal with vim 
+dnf update 
+
+# router build need all these except for git and vim) 
+dnf -y install git vim autoconf automake libtool libtool-ltdl \
+libtool-ltdl-devel ncurses-devel glibc.i686 elfutils-libelf.i686 \
+libstdc++.i686 bison flex  gettext-devel gperf byacc intltool gcc-c++ \ 
+zlib-devel 
